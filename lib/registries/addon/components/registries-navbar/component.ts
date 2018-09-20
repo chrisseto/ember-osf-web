676ae0f6649aa0cb29c8cdbfaeff4ac460094be4
Chris Seto <chriskseto@gmail.com>
2018-09-20 14:52:43 -0400

    Close to all the navbar styling

--- lib/registries/addon/components/registries-navbar/component.ts
+++ lib/registries/addon/components/registries-navbar/component.ts
@@ -1,35 +1,35 @@
 import { layout, tagName } from '@ember-decorators/component';
 import { action } from '@ember-decorators/object';
+import { and } from '@ember-decorators/object/computed';
 import { service } from '@ember-decorators/service';
-import Component from '@ember/component';
 import requiredAction from 'ember-osf-web/decorators/required-action';
-import CurrentUser from 'ember-osf-web/services/current-user';
-import Session from 'ember-simple-auth/services/session';
+import Media from 'ember-responsive';
+import { AuthBase } from 'osf-components/components/osf-navbar/auth-dropdown/component';
 import { OSF_SERVICES } from 'osf-components/components/osf-navbar/component';
 import template from './template';
 
 @tagName('')
 @layout(template)
-export default class NavBar extends Component {
-    @service currentUser!: CurrentUser;
-    @service session!: Session;
+export default class NavBar extends AuthBase {
+    @service media!: Media;
 
     @requiredAction onSearch!: (query: string) => void;
+    @and('media.isMobile', 'searchDropdownOpen') showSearchDropdown!: boolean;
 
     services = OSF_SERVICES;
     helpRoute = 'http://help.osf.io/m/registrations/';
     donateRoute = 'https://cos.io/donate';
 
-    showMobileSearchBox: boolean = false;
+    searchDropdownOpen: boolean = false;
 
     @action
-    _onSearch(query: string) {
+    _onSearch(this: NavBar, query: string) {
         this.onSearch(query);
-        this.set('showMobileSearchBox', false);
+        this.set('searchDropdownOpen', false);
     }
 
     @action
     toggleMobileSearch() {
-        this.set('showMobileSearchBox', !this.showMobileSearchBox);
+        this.set('searchDropdownOpen', !this.searchDropdownOpen);
     }
 }
